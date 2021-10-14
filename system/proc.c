7d17fd0673acdd7e18897a17b748a754bd9e1ad1
ZH L <zhl@ZHdeMacBook-Air.local>
2021-10-15 00:44:20 +0800

    update

--- system/proc.c
+++ system/proc.c
@@ -7,4 +7,57 @@
 #include "proc.h"
 #include "spinlock.h"
 
-struct  
+struct {
+  struct spinlock lock;
+  struct proc proc[NPROC];
+} ptable;
+
+static struct proc *initproc;
+
+int32 nextpid = 1;
+extern void forkret(void);
+extern void trapret(void);
+
+static void wakeup1(void *chan);
+
+void
+print(void) {
+  initlock(&ptable.lock, "ptable");
+}
+
+
+// 必须要在关闭中断的情况下调用
+// TODO 为什么是 - 号
+int32
+cpuid() {
+  return mycpu()-cpus;
+}
+
+struct cpu*
+mycpu(void) {
+  int32 apicid, i;
+
+  if (readeflags() & FL_IF)
+    panic("mycpu called with interrupts enabled\n");
+
+  apicid = lapicid();
+
+  for (i = 0; i < ncpu; ++i) {
+    if (cpus[i].apicid == apicid)
+      return &cpus[i];
+  }
+  panic("unknown apicid\n");
+}
+
+struct proc*
+myproc(void) {
+  struct cpu *c;
+  struct proc *p;
+  pushcli();
+  c = mycpu();
+  p = c->proc;
+  popcli();
+  return p;
+}
+
+
